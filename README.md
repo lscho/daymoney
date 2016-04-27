1aec9bc2a68b853cf7d0e3554e01718294807c50
eyblog <admin@eyblog.com>
2016-04-27 10:38:59 +0800

    add demo1

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 ##使用
 
+**日期选择**
+```javascript
+            $('input').click(function(){
+                $("#calendar").daymoney({
+                    'click':function(obj){
+                        $("#input").val(obj.data('date'));
+                        $("#calendar").hide();
+                    }
+                }).init();
+            });
+```
+
+**日历价格**
+
 ```javascript
             var daydata = '[{"day":"2016-04-10|228"},{"day":"2016-04-11|228"},{"day":"2016-04-12|228"},{"day":"2016-04-13|228"},{"day":"2016-04-14|258"},{"day":"2016-04-15|228"},{"day":"2016-04-16|228"},{"day":"2016-04-17|308"},{"day":"2016-04-19|228"},{"day":"2016-04-20|228"},{"day":"2016-04-22|228"},{"day":"2016-04-23|228"},{"day":"2016-04-24|228"},{"day":"2016-04-25|228"},{"day":"2016-04-26|228"},{"day":"2016-04-27|228"},{"day":"2016-04-28|558"},{"day":"2016-04-29|228"},{"day":"2016-04-30|228"},{"day":"2016-04-31|228"}]';
             var daymoney=$("#calendar").daymoney({
                 daydata:daydata,    //日期价格数据
                 events:'click',     //监听事件，默认为click
-                'prev':'btnPre',    //上一月
-                'next':'btnNext',   //下一月
                 'style':{
                     disabled:"am-disabled",     //禁用日期样式
                     active:"am-active",         //激活日期样式
--- dlist/daymoney.js
+++ dlist/daymoney.js
@@ -2,22 +2,22 @@
     var daymoney = function(data) {
             this.daydata = data.daydata;
             this.aMonthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-            this.oDate = new Date();
+            this.oDate = data.date?new Date(data.date):new Date();
             this.iYear = this.oDate.getFullYear();
             this.iMonth = this.oDate.getMonth();
             this.iDay = this.oDate.getDate();
-            this.oCalendar = document.getElementById(data.id);
-            this.aTd = this.oCalendar.getElementsByTagName('td');
-            this.oBtnPre = $(".am-daymoney-prev")[0];
-            this.oBtnNext = $(".am-daymoney-next")[0];
+            this.oCalendar = $('#'+data.id)[0];
+            this.aTd = $('#'+data.id+' td');
+            this.oBtnPre = $('#'+data.id+' .am-daymoney-prev')[0];
+            this.oBtnNext = $('#'+data.id+' .am-daymoney-next')[0];
             this.oTime = document.getElementById('time');
             this.events = data.events;
-            this.style=data.style;
-            this.load=data.load;
-            this.click=data.click;
+            this.style = data.style;
+            this.load = data.load;
+            this.click = data.click;
         }
         //上一月
-    daymoney.prototype._prev = function() {
+    daymoney.prototype.prev = function() {
             if (this.iMonth == 0) {
                 this.iMonth = 11
                 this.iYear--;
@@ -27,40 +27,60 @@
             this._loadDate();
         }
         //下一月
-    daymoney.prototype._next = function() {
-            if (this.iMonth == 11) {
-                this.iMonth = 0;
-                this.iYear++;
-            } else {
-                this.iMonth++;
-            }
-            this._loadDate();
+    daymoney.prototype.next = function() {
+        if (this.iMonth == 11) {
+            this.iMonth = 0;
+            this.iYear++;
+        } else {
+            this.iMonth++;
         }
-        //初始化
+        this._loadDate();
+    }
+    daymoney.prototype.setmoney=function(day,money,callback){
+        var data=this.daydata;
+        var is=false;
+        for(var o in data){
+            if(data[o].day.indexOf(day)>-1){
+                data[o].day=day+'|'+money;
+                is=true;
+            }
+        }
+        if(!is){
+            data.push({day:day+'|'+money});
+        }
+        this._loadDate();
+        callback({day:day,money:money});
+    }
+    //初始化
     daymoney.prototype.init = function() {
             var oBtnPre = this.oBtnPre;
             var oBtnNext = this.oBtnNext;
             var events = this.events;
-            var load=this.load;
-            var click=this.click;
-            var td=this.aTd;
+            var load = this.load;
+            var click = this.click;
+            var td = this.aTd;
             var self = this;
             this._loadDate();
             //注册事件
             if (oBtnPre) {
                 oBtnPre.addEventListener(events, function() {
-                    self._prev()
+                    self.prev()
                 });
             }
             if (oBtnNext) {
                 oBtnNext.addEventListener(events, function() {
-                    self._next();
+                    self.next();
                 });
             }
-            $(td).on(events,function(){
-                click($(this));
-            });
-            load(this);
+            if(click){
+                $(td).on(events, function() {
+                    click($(this));
+                });
+            }
+            if(load){
+                load(this);
+            }
+            self.oCalendar.style.display='block';
             return this;
         }
         //加载日历
@@ -100,8 +120,8 @@
         var iWeek = new Date(iYear, iMonth, 1).getDay(); //获得本月第一天是星期几
         var iCurDays = getMonthDays(iYear, iMonth); //获得本月天数
         var iPreDays = getMonthDays(iYear, iMonth - 1); //获得上月天数
-        document.getElementById('yearNum').innerHTML = iYear;
-        document.getElementById('monthNum').innerHTML = iMonth + 1;
+        $('.am-daymoney-year').html(iYear);
+        $('.am-daymoney-month').html(iMonth + 1);
         var data = [],
             y_list = [],
             m_list = [],
@@ -140,10 +160,10 @@
             var this_m = (m_list[i] > 9) ? m_list[i] : '0' + m_list[i];
             var this_d = (d_list[i] > 9) ? d_list[i] : '0' + d_list[i];
             aTd[i].setAttribute('data-date', this_y + '-' + this_m + '-' + this_d);
-            if(new Date(this_y + '-' + this_m + '-' + this_d)>new Date()){
-                aTd[i].className=this.style.active;
-            }else if(new Date(this_y + '-' + this_m + '-' + this_d)<new Date()){
-                aTd[i].className=this.style.disabled;
+            if (new Date(this_y + '-' + this_m + '-' + this_d) > new Date()) {
+                aTd[i].className = this.style.active;
+            } else if (new Date(this_y + '-' + this_m + '-' + this_d) < new Date()) {
+                aTd[i].className = parseInt(Math.abs(new Date(this_y + '-' + this_m + '-' + this_d) - new Date()) / 1000 / 60 / 60 / 24) != 0 ? this.style.disabled : this.style.today;
             }
             aTd[i].innerHTML = data[i];
             aTd[i].index = i;
@@ -166,31 +186,35 @@
                             mo = 0 + mo;
                         }
                         aTd[i].setAttribute('data-money', money);
-                        aTd[i].innerHTML = '<span id="dhx_month_head">' + da + '</span><br/><span id="dhx_month_body">￥' + money + '</span>';
+                        aTd[i].innerHTML = '<span class="am-daymoney-month-head">' + da + '</span><br/><span class="daymoney-month-body">￥' + money + '</span>';
                     }
                 }
             }
         }
     }
     $.fn.daymoney = function(data) {
-        var html='<table class="am-table am-table-bordered"><caption><i class="am-daymoney-prev"><<</i><span id="yearNum"></span>年<span id="monthNum"></span>月<i class="am-daymoney-next">>></i></caption><thead><tr><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th class="red">六</th><th class="red">日</th></tr></thead><tbody>';
-        for(var i=0;i<6;i++){
-            html+='<tr>';
-            for(var t=0;t<7;t++){
-                html+='<td></td>';
+        var html = '<table class="am-table am-table-bordered"><caption><i class="am-daymoney-prev"><<</i><span class="am-daymoney-year"></span>年<span class="am-daymoney-month"></span>月<i class="am-daymoney-next">>></i></caption><thead><tr><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th class="red">六</th><th class="red">日</th></tr></thead><tbody>';
+        for (var i = 0; i < 6; i++) {
+            html += '<tr>';
+            for (var t = 0; t < 7; t++) {
+                html += '<td></td>';
             }
-            html+='</tr>';
+            html += '</tr>';
         }
-        html+='</tbody></table>';
+        html += '</tbody></table>';
         this.html(html);
-        if(!data)data=new object();
-        data.id=this.attr('id');
-        data.daydata=(typeof data.daydata=='object')?data.daydata:JSON.parse(data.daydata);
-        data.events=data.events?data.events:'click';
-        if(!data.style)data.style={};
-        data.style.disabled=data.style.disabled?data.style.disabled:"am-disabled";
-        data.style.active=data.style.active?data.style.active:"am-active";
-        data.style.today=data.style.today?data.style.today:"am-primary";
+        if (!data) data = {};
+        data.id = this.attr('id');
+        if(data.daydata){
+            data.daydata = (typeof data.daydata == 'object') ? data.daydata : JSON.parse(data.daydata);
+        }else{
+            data.daydata={};
+        }
+        data.events = data.events ? data.events : 'click';
+        if (!data.style) data.style = {};
+        data.style.disabled = data.style.disabled ? data.style.disabled : "am-disabled";
+        data.style.active = data.style.active ? data.style.active : "am-active";
+        data.style.today = data.style.today ? data.style.today : "am-primary";
         return new daymoney(data);
     }
 })(jQuery);
